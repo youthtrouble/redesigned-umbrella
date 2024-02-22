1006c6267928bfa549ee1fb7bf04e0621356d103
Deji Ajibola <harjhibolar@gmail.com>
2024-02-22 15:40:07 +0000

    chore : add methods to test queue on main execution

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/youthtrouble/redesigned-umbrella/broker"
+)
+
+func main() {
+
+	rabbitMQConfig, err := broker.NewRabbitMQConfig()
+	if err != nil {
+		log.Fatalf("Error creating RabbitMQ config: %s", err)
+	}
+
+	_, err = broker.NewQueue(rabbitMQConfig.QueueChannel) // not ready to be used
+	if err != nil {
+		log.Fatalf("Error creating main queue: %s", err)
+	}
+}
